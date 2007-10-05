2df720e85ce026aef66a3ae4f46ca8ab93d377d8
tassoman <tassoman@a65382f9-2321-0410-b7af-d1673e31a2e7>
2007-10-05 07:52:16 +0000

    "Resolved data check problem Rnd resolved email check problem"

--- trunk/fcc/parse_form.php
+++ trunk/fcc/parse_form.php
@@ -4,33 +4,33 @@ class fcc_parseForm
 	public $error_msg = '';
 	public $error = false;
 	public $error_input = array();
-	
+
 
 	/**
 	 * check if the input is an integer
 	 *
 	 * @param mixed $date
 	 * @return boolean
-	 */	
+	 */
 	public function integer($integer)
-	{	
+	{
 		foreach ($integer as $k => $v)
 		{
 			$num = intval($_POST['fcc'][$v]);
-			
+
 			if ($_POST['fcc'][$v] != "$num")
 			{
 				$this->error_msg .= '<li>' . sprintf( __("field %s is not numeric", 'fcc') , $v) . '</li>' . "\n";
 				$this->error_input[] = $v;
-				$this->error = true;							
-				
+				$this->error = true;
+
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 
 	/**
 	 * check if the input is a valid date (dd-mm-aaaa)
@@ -47,11 +47,11 @@ class fcc_parseForm
 				$this->error_msg .= '<li>' . sprintf( __( "field %s is not a valid date (dd-mm-yyyy)" , 'fcc') , $v ) . '</li>' . "\n";
 				$this->error_input[] = $v;
 				$this->error = true;
-				
+
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
 
@@ -65,19 +65,19 @@ class fcc_parseForm
 	{
 		foreach ($tel as $k => $v)
 		{
-			if (ereg("^[00[1-9]{1,4}|\+[1-9]{1,4}]?[[:blank:]\./-]?(3[2-9][0-9]|0[2-9][0-9]{1,2})[[:blank:]\./-]?[0-9]{6,9}$|^$",$_POST['fcc'][$v]))
+			if ( ! ereg("^[00[1-9]{1,4}|\+[1-9]{1,4}]?[[:blank:]\./-]?(3[2-9][0-9]|0[2-9][0-9]{1,2})[[:blank:]\./-]?[0-9]{6,9}$|^$",$_POST['fcc'][$v]))
 			{
 				$this->error_msg .= '<li>' . sprintf ( __("field %s is not a valid telephone number (+xx-xxxx-xxxxxxxx)") , $v ) . '</li>' . "\n";
 				$this->error_input[] = $v;
 				$this->error = true;
-				
+
 				return false;
 			}
 		}
-		
+
 		return true;
-	}	
-	
+	}
+
 	/**
 	 * controllo che il campo non sia vuoto
 	 *
@@ -93,14 +93,14 @@ class fcc_parseForm
 				$this->error_msg .= '<li>' . sprintf( __("field %s is empty" , 'fcc') , $v ) . '</li>' . "\n";
 				$this->error_input[] = $v;
 				$this->error = true;
-				
+
 				return false;
 			}
 		}
-		
-		return true;		
+
+		return true;
 	}
-	
+
 	/**
 	 * controllo che il campo sia una email valida
 	 *
@@ -111,22 +111,23 @@ class fcc_parseForm
 	{
 		foreach ($emails as $k => $v)
 		{
-			if (ereg("^a-z0-9@.",$_POST['fcc'][$v]))
+#			if (ereg("^a-z0-9@.", $emails))
+			if ( ! ereg('^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$', $_POST['fcc'][$v]))
 			{
 				$this->error_msg .= '<li>' . sprintf( __("field %s is not a valid email" , 'fcc') , $v ) . '</li>' . "\n";
 				$this->error_input[] = $v;
 				$this->error = true;
-				
+
 				return false;
 			}
 		}
-		
-		return true;		
+
+		return true;
 	}
-	
+
 	/**
 	 * check if the value is > of the input check
-	 * if the input data  type is a string then 
+	 * if the input data  type is a string then
 	 * the max is checked on the string lenght
 	 *
 	 * @param mixed $max
@@ -137,34 +138,34 @@ class fcc_parseForm
 		foreach ($max as $num => $v)
 		{
 			$campo = explode(',',$v);
-			
+
 			foreach ($campo as $id => $valore)
 			{
-				
+
 				$toNum = intval($_POST['fcc'][$valore]);
-							
+
 				if ("$toNum" != $_POST['fcc'][$valore])
 					$checker = strlen($_POST['fcc'][$valore]);
-				else 
-					$checker = $toNum;	
-							
+				else
+					$checker = $toNum;
+
 				if ( $checker > $num)
 				{
 					$this->error_msg .= '<li>' . sprintf( __("value %s is greater than max allowed value (%d)" , 'fcc') , $valore, $num ) . '</li>' . "\n";
 					$this->error_input[] = $valore;
 					$this->error = true;
-					
+
 					return false;
 				}
 			}
 		}
-		
-		return true;		
+
+		return true;
 	}
 
 	/**
 	 * check if the value is < of the input check
-	 * if the input data  type is a string then 
+	 * if the input data  type is a string then
 	 * the min is checked on the string lenght
 	 *
 	 * @param mixed $min
@@ -178,26 +179,26 @@ class fcc_parseForm
 			foreach ($campo as $id => $valore)
 			{
 				$toNum = intval($_POST['fcc'][$valore]);
-				
+
 				if ("$toNum"!=$_POST['fcc'][$valore])
 					$checker = strlen($_POST['fcc'][$valore]);	//is a string
-				else 
+				else
 					$checker = $toNum;	//is a integer
-							
+
 				if ( $checker < $num)
 				{
 					$this->error_msg .= '<li>' . sprintf( __("value %s is smaller than min allowed value (%d)" , 'fcc') , $valore, $num ) . '</li>' . "\n";
 					$this->error_input[] = $valore;
 					$this->error = true;
-					
+
 					return false;
 				}
 			}
 		}
-		
-		return true;		
+
+		return true;
 	}
-	
+
 }
 
 ?>
